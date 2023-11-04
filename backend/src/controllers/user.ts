19f2a52abc1eb6953497123c7d77817634995697
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2023-11-04 20:43:43 +0000

    add signup and login validators

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -16,9 +16,34 @@ const getAllusers = async (req: Request, res: Response, next: NextFunction) => {
 const userSignup = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(401).send('User already registered');
+
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
+
+    res.status(201).json({ message: 'OK', id: user._id.toString() });
+  } catch (error) {
+    console.log('catch error => ', error);
+    res.status(500).json({ message: 'ERROR', cause: error.message });
+  }
+};
+
+const userLogin = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send('User not registered');
+    }
+
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(403).send('Incorrect Password');
+    }
 
     res.status(200).json({ message: 'OK', id: user._id.toString() });
   } catch (error) {
@@ -27,4 +52,4 @@ const userSignup = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { getAllusers, userSignup };
+export { getAllusers, userSignup, userLogin };
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
-import { getAllusers, userSignup } from '../controllers/user.js';
+import { getAllusers, userLogin, userSignup } from '../controllers/user.js';
+import {
+  loginValidator,
+  signupValidator,
+  validate,
+} from '../utils/validators.js';
 
 const userRoutes = Router();
 
 userRoutes.get('/', getAllusers);
-userRoutes.post('/signup', userSignup);
+userRoutes.post('/signup', validate(signupValidator), userSignup);
+userRoutes.post('/login', validate(loginValidator), userLogin);
 
 export { userRoutes };
